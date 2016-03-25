9995f9b7e9d2208cc5b0c318b5708c448d84a776
caolh <444261689@qq.com>
2016-03-25 10:49:29 +0800

    require测试demo

---
+++ require/helloe.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var s = 'Hello';
+
+function greet(name) {
+    console.log(s + ', ' + name + '!');
+}
+
+module.exports = greet;
+
+(function () {
+    // 读取的hello.js代码:
+    var s = 'Hello';
+    var name = 'world';
+
+    console.log(s + ' ' + name + '!');
+    // hello.js代码结束
+})();
+
+
+// 准备module对象:
+var module = {
+    id: 'hello',
+    exports: {}
+};
+var load = function (module) {
+    // 读取的hello.js代码:
+    function greet(name) {
+        console.log('Hello, ' + name + '!');
+    }
+
+    module.exports = greet;
+    // hello.js代码结束
+    return module.exports;
+};
+var exported = load(module);
+
+// 保存module:
+//save(module, exported);
---
+++ require/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// 引入hello模块:
+var greet = require('./helloe');
+
+var s = 'Michael';
+
+greet(s); // Hello, Michael!
+
+var variable = 'caolh1';
+console.log(variable);
